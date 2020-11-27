e6c5ba069dba7d2c9d9699833f9d791892fb3f5c
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2020-11-27 22:05:35 +0100

    favicon, title, colors

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 ### Deploy
 
 ```
+npm run build
 firebase deploy
 ```
 
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,10 @@
+export const globalStyles = {
+    colors: {
+        grayLight2: "#f5f5f5",
+        grayLight: "#d6d8d7",
+        gray: "#cccccc",
+        grayDark: "#333333",
+        primary: "#00ffd0",
+        secondary: "#f58400",
+    },
+};
