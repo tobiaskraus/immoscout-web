298f25d29ce2f9260ee0656b926e879cff20320d
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2020-11-27 22:49:15 +0100

    Card: show specific rows & otherRows (JSON)

--- src/components/CardProperty.tsx
+++ src/components/CardProperty.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Property } from "../models/Property";
+import { globalStyles } from "../styles/globalStyles";
 
 interface CardPropertyProps {
     className?: string;
@@ -11,6 +12,19 @@ export const CardProperty = (props: CardPropertyProps) => {
     return (
         <Card>
             <h2>{props.property.title}</h2>
+            <div>
+                <Rows {...props} />
+                <hr />
+                <OtherRows
+                    {...props}
+                    excludeKey={[
+                        "sqm_wohnflaeche",
+                        "price_total",
+                        "title",
+                        "images",
+                    ]}
+                />
+            </div>
         </Card>
     );
 };
@@ -21,4 +35,52 @@ const Card = styled.div`
     margin-bottom: 5px;
     padding: 10px 20px;
     box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const Rows = (props: CardPropertyProps) => (
+    <div>
+        <Row>
+            <Label>Wohnfläche</Label>
+            <Value>{props.property.sqm_wohnflaeche}</Value>
+        </Row>
+        <Row>
+            <Label>Preis Netto</Label>
+            <Value>{props.property.price_net}</Value>
+        </Row>
+        <Row>
+            <Label>Preis Total</Label>
+            <Value>{props.property.price_total}</Value>
+        </Row>
+    </div>
+);
+
+type OtherRowsProps = CardPropertyProps & { excludeKey: string[] };
+const OtherRows = (props: OtherRowsProps) => {
+    const rows = Object.entries(props.property).filter(
+        ([key]) => props.excludeKey.indexOf(key) === -1
+    );
+    return (
+        <>
+            {rows.map(([key, value]) => (
+                <Row>
+                    <Label>{key}</Label>
+                    <Value>{JSON.stringify(value)}</Value>
+                </Row>
+            ))}
+        </>
+    );
+};
+
+const Row = styled.div`
+    display: flex;
+`;
+
+const Label = styled.div`
+    flex: 0 0 30%;
+    color: ${globalStyles.colors.textLight};
+`;
+
+const Value = styled.div`
+    flex: 0 0 70%;
+    color: ${globalStyles.colors.text};
 `;
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,9 +2,12 @@ export const globalStyles = {
     colors: {
         grayLight2: "#f5f5f5",
         grayLight: "#d6d8d7",
-        gray: "#cccccc",
+        gray: "#ccc",
         grayDark: "#333333",
         primary: "#00ffd0",
         secondary: "#f58400",
+
+        textLight: "#747474",
+        text: "333",
     },
 };
