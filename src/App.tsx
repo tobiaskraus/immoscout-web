f7080ce84a774cbf89a9d989fbf7697f2db72683
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-01-16 14:32:02 +0100

    add Routing (react-router-dom)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-import styled from "styled-components";
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+    Redirect,
+} from "react-router-dom";
 import { List } from "./pages/List";
 
 function App() {
     return (
-        <Page>
-            <List />;
-        </Page>
+        <Router>
+            <Switch>
+                <Route exact path="/list" component={List} />
+
+                <Route path="*">
+                    <Redirect to={{ pathname: "/list" }} />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
 export default App;
-
-const Page = styled.div`
-    background: #eaeaea;
-    padding: 20px;
-    min-height: 100%;
-`;
